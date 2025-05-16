77175047d07acdc3a0910c0723983371e29dbd86
tafu90 <46842792+tafu90@users.noreply.github.com>
2025-05-16 14:11:32 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,15 +2,18 @@
 
 
 # Start the Avahi daemon to handle mDNS service discovery
-/usr/sbin/avahi-daemon --no-daemon &
+/usr/sbin/avahi-daemon &
 
 # Fake Synology mDNS broadcast
 #avahi-publish -s "Maina" _http._tcp 5000 &
 #avahi-publish -s "Maina" _smb._tcp 445 &
 
 # Forward mDNS between local network (eth0) and VPN (wg0)
-socat UDP4-RECVFROM:5353,SO_BROADCAST,IP_ADD_MEMBERSHIP=224.0.0.251:eth0 UDP4-SENDTO:5353:wg0 &
-socat UDP4-RECVFROM:5353,SO_BROADCAST,IP_ADD_MEMBERSHIP=224.0.0.251:wg0 UDP4-SENDTO:5353:eth0 &
+##socat UDP4-RECVFROM:5353,SO_BROADCAST,IP_ADD_MEMBERSHIP=224.0.0.251:eth0 UDP4-SENDTO:5353:wg0 &
+##socat UDP4-RECVFROM:5353,SO_BROADCAST,IP_ADD_MEMBERSHIP=224.0.0.251:wg0 UDP4-SENDTO:5353:eth0 &
+
+socat UDP4-RECVFROM:5353,IP_ADD_MEMBERSHIP=224.0.0.251:eth0 UDP4-SENDTO:5353:wg0 &
+socat UDP4-RECVFROM:5353,IP_ADD_MEMBERSHIP=224.0.0.251:wg0 UDP4-SENDTO:5353:eth0 &
 
 # Keep container alive
 tail -f /dev/null
