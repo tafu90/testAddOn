4c4fe1af57ed8318d63523381fea231c4ac14a21
tafu90 <46842792+tafu90@users.noreply.github.com>
2025-05-16 14:59:12 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -20,11 +20,17 @@ export AVAHI_DAEMON_ARGS="--no-dbus"
 ##socat UDP4-RECVFROM:5353,IP_ADD_MEMBERSHIP=224.0.0.251:eth0 UDP4-SENDTO:5353:wg0 &
 ##socat UDP4-RECVFROM:5353,IP_ADD_MEMBERSHIP=224.0.0.251:wg0 UDP4-SENDTO:5353:eth0 &
 
+# Forward mDNS traffic from eth0 to wg0 (without binding to port 5353)
+socat UDP4-LISTEN:5353,fork,reuseaddr UDP4-SENDTO:5353:wg0 &
+
+# Forward mDNS traffic from wg0 to eth0 (without binding to port 5353)
+socat UDP4-LISTEN:5353,fork,reuseaddr UDP4-SENDTO:5353:eth0 &
+
 # Receive mDNS on eth0 and forward it to wg0
-socat UDP4-RECVFROM:5353,fork UDP4-SENDTO:5353:wg0 &
+##socat UDP4-RECVFROM:5353,fork UDP4-SENDTO:5353:wg0 &
 
 # Receive mDNS on wg0 and forward it to eth0
-socat UDP4-RECVFROM:5353,fork UDP4-SENDTO:5353:eth0 &
+##socat UDP4-RECVFROM:5353,fork UDP4-SENDTO:5353:eth0 &
 
 # Keep container alive
 tail -f /dev/null
