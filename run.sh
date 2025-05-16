df47502080e043fde82b49eb0ca249778b114bf4
tafu90 <46842792+tafu90@users.noreply.github.com>
2025-05-16 14:01:42 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@
 /usr/sbin/avahi-daemon --no-daemon &
 
 # Fake Synology mDNS broadcast
-avahi-publish -s "Maina" _http._tcp 5000 &
-avahi-publish -s "Maina" _smb._tcp 445 &
+#avahi-publish -s "Maina" _http._tcp 5000 &
+#avahi-publish -s "Maina" _smb._tcp 445 &
 
 # Forward mDNS between local network (eth0) and VPN (wg0)
 socat UDP4-RECVFROM:5353,SO_BROADCAST,IP_ADD_MEMBERSHIP=224.0.0.251:eth0 UDP4-SENDTO:5353:wg0 &
